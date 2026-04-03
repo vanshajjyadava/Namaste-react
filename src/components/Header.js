e0b5d68082e14d7a08d930847e4a44eccd804c84
vanshYadav <vanshajjyadava@gmail.com>
2026-04-03 16:31:21 +0530

    added toggle button for login/logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("login");
+
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src={LOGO_URL}
-        />
+        <img className="logo" src={LOGO_URL} />
       </div>
       <div className="nav-items">
         <ul>
@@ -15,6 +15,16 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login-btn"
+            onClick={() => {
+              btnName === "login"
+                ? setBtnName("logout")
+                : setBtnName("login");
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
