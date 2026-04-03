9021727bf3e35cc7af65a3171b7a8e6526ea02b0
vanshYadav <vanshajjyadava@gmail.com>
2026-04-03 14:11:16 +0530

    added Shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=29.875106556307752&lng=77.87758033722639&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+    );
+    const json = await data.json();
+    console.log(json);
+
+    setListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants || [],
+    );
+  };
+
+  if(listOfRestaurants.length === 0){
+    return <Shimmer />
+  }
 
   return (
     <div className="body">
@@ -12,7 +33,7 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
-              (res) => res.data.avgRating > 4,
+              (res) => res.info.avgRating > 4.5
             );
             setListOfRestaurants(filteredList);
           }}
@@ -22,7 +43,7 @@ const Body = () => {
       </div>
       <div className="res-container">
         {listOfRestaurants.map((restaurant) => (
-          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,5 +1,11 @@
 import { CDN_URL } from "../utils/constants";
 
+const getImageUrl = (imageId) => {
+  if (!imageId) return "https://via.placeholder.com/150";
+
+  return CDN_URL + imageId;
+};
+
 const RestaurantCard = (props) => {
   const { resData } = props;
 
@@ -9,28 +15,23 @@ const RestaurantCard = (props) => {
     avgRating,
     cuisines,
     costForTwo,
-    deliveryTime,
-  } = resData?.data;
+    sla,
+  } = resData?.info || {};
 
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
       <img
         className="res-logo"
         alt="res-logo"
-        src={
-          CDN_URL+
-          cloudinaryImageId
-        }
+        src={getImageUrl(cloudinaryImageId)}
       />
       <h3>{name}</h3>
-      <h4>{cuisines.join(", ")}</h4>
+      <h4>{cuisines?.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
-      <h4>₹{costForTwo / 100} FOR TWO</h4>
-      <h4>{deliveryTime} minutes</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla?.deliveryTime} minutes</h4>
     </div>
   );
 };
-
-
 
 export default RestaurantCard;
