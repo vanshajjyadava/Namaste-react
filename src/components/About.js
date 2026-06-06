199baf9667cf059a5dba0f6193d3c232d86b827d
vanshYadav <vanshajjyadava@gmail.com>
2026-06-06 16:42:46 +0530

    used react-router-dom for routing

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <>
+      <h1>About US!</h1>
+      <h2>This is a sample about us page.</h2>
+    </>
+  );
+};
+
+export default About;
---
+++ src/components/Error.js
@@ -0,0 +1,16 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const err = useRouteError();
+  return (
+    <>
+      <h1>Oops !</h1>
+      <h2>Something went wrong..</h2>
+      <h3>
+        {err.status}: {err.statusText}
+      </h3>
+    </>
+  );
+};
+
+export default Error;
